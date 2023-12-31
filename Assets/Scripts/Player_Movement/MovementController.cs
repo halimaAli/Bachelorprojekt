2a7029432ca168e0a5bde5c977397bd7ce5b7759
halimaAli <uk069807@student.uni-kassel.de>
2023-12-31 19:43:29 +0100

    refactor: reworked GroundChecker

---
+++ Assets/Scripts/Player_Movement/MovementController.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class MovementController : MonoBehaviour
+{
+     private float horizontalInput;
+     private float verticalInput;
+     private float walkingSpeed = 5.0f;
+     private float jumpForce = 40.0f;
+
+     [SerializeField] private LayerMask groundMask;
+     private float grounderOffset = -1;
+     private float grounderRadius = 0.5f;
+     private Collider[] _ground = new Collider[1];
+       
+     private Rigidbody rb;
+     private Animator animator;
+     private SpriteRenderer spriteRenderer;
+     [SerializeField] private BoxCollider crouchCollider;
+     [SerializeField] private BoxCollider standingCollider;
+
+     [SerializeField] private bool isCrouched;
+     [SerializeField] private bool IsGrounded;  
+
+
+    void Start()
+     {
+         rb = GetComponent<Rigidbody>();
+         animator = GetComponent<Animator>();
+         spriteRenderer = GetComponent<SpriteRenderer>();
+         isCrouched = false;
+         IsGrounded = true;
+     }
+
+     void Update()
+     {
+         horizontalInput = Input.GetAxis("Horizontal"); // A and D Input
+         verticalInput = Input.GetAxis("Vertical"); //// W and S Input
+
+         HandleWalking();
+         CheckIfGrounded();
+         HandleJumping();
+         HandleCrouching();
+     }
+
+    #region Walk
+    private void HandleWalking()
+     {
+         animator.SetBool("isWalking", Camera.main.orthographic ? horizontalInput != 0 : horizontalInput != 0 || verticalInput != 0);
+
+
+         if (Input.GetKey(KeyCode.D))
+         {
+             spriteRenderer.flipX = false;
+         }
+         else if (Input.GetKey(KeyCode.A))
+         {
+             spriteRenderer.flipX = true;
+         }
+
+         if (Camera.main.orthographic)
+         {
+             transform.Translate(Vector3.right * walkingSpeed * horizontalInput * Time.deltaTime);
+
+         }
+         else
+         {
+             transform.Translate(Vector3.right * walkingSpeed * horizontalInput * Time.deltaTime);
+             transform.Translate(Vector3.forward * walkingSpeed * verticalInput * Time.deltaTime);
+         }
+     }
+    #endregion
+
+    #region GroundCheck
+
+    private void CheckIfGrounded()
+    {
+        bool grounded = Physics.OverlapSphereNonAlloc(transform.position + new Vector3(0, grounderOffset), grounderRadius, _ground, groundMask) > 0; //checks if player hits a collider on layer "Ground"
+
+        if (!IsGrounded && grounded) //if player hits Ground Layer but IsGround variable is still false
+        {
+            IsGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
+        else if (IsGrounded && !grounded) //if player is not grounded but IsGround variable is still true
+        {
+            IsGrounded = false;
+            animator.SetBool("isJumping", true);
+            transform.SetParent(null);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //draws a sphere at the feet of player; helpful in scene view
+        Gizmos.DrawWireSphere(transform.position + new Vector3(0, grounderOffset), grounderRadius); 
+    }
+
+    #endregion
+
+    #region Jump
+
+    private void HandleJumping()
+       {
+           if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+           {
+               rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
+           }
+       }
+    #endregion
+
+    #region Crouch
+    private void HandleCrouching()
+     {
+         if (Input.GetKeyDown(KeyCode.LeftShift))
+         {
+             if (!isCrouched)
+             {
+                 animator.SetBool("isCrouching", true);
+                 standingCollider.enabled = false;
+
+                 crouchCollider.enabled = true;
+                 isCrouched = true;
+             }
+             else
+             {
+                 animator.SetBool("isCrouching", false);
+                 standingCollider.enabled = true;
+
+                 crouchCollider.enabled = false;
+                 isCrouched = false;
+             }
+
+         }
+     }
+    #endregion
+}
--- Assets/Scripts/Player_Movement/PlayerController.cs
+++ Assets/Scripts/Player_Movement/PlayerController.cs
@@ -4,18 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float horizontalInput;
-    public float verticalInput;
-    public float speed;
-
-    public Rigidbody rb;
-    public float jumpForce = 10.0f;
-    public bool isGrounded;
-    public bool isCrouched;
-
     Animator animator;
-    SpriteRenderer spriteRenderer;
     public BoxCollider crouchCollider;
     public BoxCollider standingCollider;
 
@@ -26,11 +15,8 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        isGrounded = true;
-        isCrouched = false;
         SetRespawnPoint(transform.position);
     }
 
@@ -51,76 +37,7 @@ public class PlayerController : MonoBehaviour
             Die(true);
             return;
         }
-        
-        //move left and right
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
 
-        TriggerWalkAnimation(Camera.main.orthographic);
-
-        if (Camera.main.orthographic)
-        {
-            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
-        }
-        else
-        {
-            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
-            transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
-        }
-
-        //Jump
-        Jump();
-        Crouch();
-
-        //check if player fell off plattform
-    }
-
-    private void TriggerWalkAnimation(bool is2D)
-    {
-        animator.SetBool("isMoving", is2D ? horizontalInput != 0 : horizontalInput != 0 || verticalInput != 0);
-
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            spriteRenderer.flipX = false;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            spriteRenderer.flipX = true;
-        }
-    }
-
-    private void Jump()
-    {
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-        {
-            animator.SetBool("isJumping", true);
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
-    }
-
-    private void Crouch() 
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift)) 
-        {
-            if (!isCrouched)
-            {
-                animator.SetBool("isCrouching", true);
-                standingCollider.enabled = false;
-
-                crouchCollider.enabled = true;
-                isCrouched = true;
-            } else
-            {
-                animator.SetBool("isCrouching", false);
-                standingCollider.enabled = true;
-
-                crouchCollider.enabled = false;
-                isCrouched = false;
-            }
-            
-        }
     }
 
     private void MiniJump()
@@ -158,22 +75,5 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isMoving", false);
         animator.SetBool("isDead", false);
         MiniJump();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag.Equals("ground"))
-        {
-            isGrounded = true;
-            animator.SetBool("isJumping", false);
-        }
-
-        if  (other.gameObject.tag.Equals("Enemy"))
-        {
-          /*  animator.SetBool("isDead", true);
-            standingCollider.enabled = false;
-            crouchCollider.enabled = false;*/
-            
-        }
     }
 }
