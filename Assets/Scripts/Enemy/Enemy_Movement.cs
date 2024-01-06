06336a191934b041702ec5cd6da61d5bbbc09c47
halimaAli <uk069807@student.uni-kassel.de>
2024-01-06 20:35:12 +0100

    feat: added slime enemy to tutorial

--- Assets/Scripts/Enemy/Enemy_Movement.cs
+++ Assets/Scripts/Enemy/Enemy_Movement.cs
@@ -2,21 +2,50 @@ using UnityEngine;
 
 public class Enemy_Movement : MonoBehaviour
 {
-    public int speed;
+    private int speed = 1;
+    [SerializeField] private int jumpForce = 3;
 
-    int direction = 1;
+    private int direction = 1;
+    private bool isGrounded = true;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * direction *  Time.deltaTime);
+        Jump();
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    #region Jump
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    #endregion
+
+    #region Left and Right Movement
+    private void MoveLeftandRight()
+    {
+        transform.Translate(Vector3.right * speed * direction *  Time.deltaTime);
+    }
+ #endregion
+  
+    private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag.Equals("ground"))
         {
             direction *= -1;
-        }
+            isGrounded = true;
+        } 
     }
 }
