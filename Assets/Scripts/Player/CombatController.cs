68e910bf10ae9f4bf998fc0ae6d1789dbf4dfdd4
halimaAli <uk069807@student.uni-kassel.de>
2024-01-17 20:01:42 +0100

    refactor: reworked projectile
    - added new sprite
    - projectile is visible in both views
    - projectile destroys itself after 1 sec, when it becomes unvisbible or when it hits smth

--- Assets/Scripts/Player/CombatController.cs
+++ Assets/Scripts/Player/CombatController.cs
@@ -1,30 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class CombatController : MonoBehaviour
 {
 
     [SerializeField] GameObject projectilePrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
         HandleShooting();
-
     }
 
     private void HandleShooting()
     {
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            if (Camera.main.orthographic)
+            {
+                projectilePrefab.transform.rotation = Quaternion.Euler(0,0,0);
+            }
+            else
+            {
+                projectilePrefab.transform.rotation = Quaternion.Euler(0, 90, 0);
+            }
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
+        }     
     }
 }
--- Assets/Scripts/Player/Projectile/ProjectileController.cs
+++ Assets/Scripts/Player/Projectile/ProjectileController.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    [SerializeField] private float speed = 40.0f;
+
+    [Range(1, 20)]
+    [SerializeField] private float speed = 20.0f;
+    private float lifeTime = 1.0f;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (Camera.main.orthographic)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        } else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (!spriteRenderer.isVisible)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //else destroy after 3 secs
+        Destroy(gameObject, lifeTime);
     }
 
-
-
+    //Destroy if it hits wall or ground
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
