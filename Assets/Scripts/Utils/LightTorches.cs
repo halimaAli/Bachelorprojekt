07aae98b1eef77b71250ad7ec180858edb0bf59b
halimaAli <uk069807@student.uni-kassel.de>
2024-04-01 20:03:16 +0200

    feat: added torches as respawn points

---
+++ Assets/Scripts/Utils/LightTorches.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTorches : MonoBehaviour
+{
+    private Animator ani;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ani.SetBool("on", true);
+    }
+}
