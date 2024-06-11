02c4fe7a4952c33beda7b5d4a59a998ef123d94d
halimaAli <uk069807@student.uni-kassel.de>
2024-06-11 18:51:57 +0200

    feat: added Level Exit Portal

---
+++ Assets/Scripts/Utils/LevelExit.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private bool enterAllowed;
+    public string nextLevel;
+    [SerializeField] private GameObject toolTip;
+
+    private void Start()
+    {
+        if (toolTip != null)
+        {
+            toolTip.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (enterAllowed && Input.GetKeyDown(KeyCode.Return))
+        {
+            if (string.IsNullOrEmpty(nextLevel))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            } else
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (toolTip != null)
+            {
+                toolTip.SetActive(true);
+            }
+            enterAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (toolTip != null)
+            {
+                toolTip.SetActive(false);
+            }
+            enterAllowed = true;
+        }
+    }
+}
